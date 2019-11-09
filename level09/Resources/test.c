b09325584f151bc628f512b38faf4e8b5bc4c027
Nicolas IRAGNE <niragne@e3r12p8.42.fr>
2019-11-09 05:11:06 +0100

    nn

---
+++ level09/Resources/test.c
@@ -0,0 +1,17 @@
+#include <fcntl.h>
+#include <unistd.h>
+main()
+{
+	char buf[256];
+	int fd = open("./token", O_RDONLY);
+
+	int i = 0;
+	int j = read(fd, buf, 256);
+
+	while (i < j)
+	{
+		printf("%c", buf[i] - i);
+		i++;
+	}
+	printf("\n");
+}
